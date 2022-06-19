180c5d46d9819f22c2b78ac85db49ee400492753
Terrence Tyson <tsqware@gmail.com>
2022-06-19 08:07:03 -0400

    feat(web-app): create api to get all todos

---
+++ fullstack-react-app/pages/api/todos.ts
@@ -0,0 +1,31 @@
+import { getSession } from "next-auth/react"
+import { PrismaClient } from "@prisma/client"
+import { NextApiRequest, NextApiResponse } from "next"
+import { Session } from "next-auth"
+import { UserSession } from "./auth/[...nextauth]"
+
+const prisma = new PrismaClient()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	
+	if (req.method !== "GET") {
+		res.status(405).json("Method not allowed")
+		return
+	}
+
+	const session: Session | null = await getSession({req})
+
+	if (!session) {
+		res.status(401).send("Unauthorized")
+		return
+	}
+	
+	const userSession: UserSession = session as UserSession
+	const todos = await prisma.todo.findMany({
+		where: {
+			userId: userSession.userId
+		}
+	})
+
+	return res.json(todos)
+}
